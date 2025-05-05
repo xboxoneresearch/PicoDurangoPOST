7c9fb2796d1f307e14000ce7348dc8fff8c26441
tuxuser <462620+tuxuser@users.noreply.github.com>
2025-05-05 03:17:38 +0200

    chore: Refactor, move structs, enums and helpers into common.h

--- src/common.h
+++ src/common.h
@@ -6,8 +6,160 @@
 
 #define __DEBUG__
 
+// If you need to use different I/O pins for I2C, change it here
+#define PIN_SDA_XBOX 0 // GPIO0
+#define PIN_SCL_XBOX 1 // GPIO1
+// ---
+
+/* DISPLAY */
+#define PIN_SDA_DISP 6 // GPIO6
+#define PIN_SCL_DISP 7 // GPIO7
+#define DISP_SCREEN_WIDTH 128
+#define DISP_SCREEN_HEIGHT 32
+#define SSD1306_DISP_ADDRESS 0x3C
+
+/* MAX6958 emulation */
+#define MAX6958_ADDRESS 0x38
+#define MAX6958_REGISTER_SIZE 0x25
+
+/* POST Code storage */
+#define POST_MAX_QUEUE_SIZE 30
+
+#define CTRL_C 3
+
 #if defined(__DEBUG__)
 #define DBG(format, ...) Serial.printf("[DBG] "#format"\r\n", ##__VA_ARGS__)
 #else
 #define DBG(x, ...)
 #endif
+
+// State machine states
+enum State {
+    STATE_RETURN_TO_REPL,
+    STATE_REPL,
+    STATE_POST_MONITOR,
+    STATE_I2C_SCAN
+};
+
+typedef struct {
+    uint16_t code;
+    const char *name;
+} PostCode, *PPostCode;
+
+typedef struct {
+    uint8_t address;
+    const char *name;
+} I2cDevice, *PI2cDevice;
+
+static I2cDevice KnownDevices[] = {
+    {0x15, "V_NBCORE"},
+    {0x16, "V5_P0"},
+    {0x18, "V_MEMIOAB"},
+    {0x19, "V_MEMIOCD"},
+    {0x20, "V_GFXCORE / V_CPUCORE"},
+    {0x21, "V_MEMPHY/V_MEMIO/V_SOC"},
+    {0x24, "GPIO EXPANDER (UNUSED)"},
+
+    {0x2D, "V_DRAM1P8 DEBUG (UNUSED)"},
+    {0x2E, "V_SOC1P8 DEBUG (UNUSED)"},
+    {0x2F, "V_SOCPHY DEBUG (UNUSED)"},
+
+    {0x40, "RAMVPP MONITOR (UNUSED)"},
+    {0x41, "M2 MONITOR (UNUSED)"},
+    {0x42, "CFEXPRESS MONITOR (UNUSED)"},
+    {0x44, "12P0 MONITOR (UNUSED)"},
+    {0x45, "12P0_SB MONITOR (UNUSED)"},
+    {0x4A, "V_SOC1P8 MONITOR (UNUSED)"},
+    {0x4B, "V_SOCPHY MONITOR (UNUSED)"},
+
+    {0x38, "MAX6958/9A"},
+    {0x39, "MAX6958/9B"},
+    
+    {0x5A, "RF Unit (ISD9160)"},
+    {NULL, NULL}
+};
+
+static PostCode PostCodes[] = {
+    {0x14FF, "SUCCESS"},
+    {0x0075, "SUCCESS"},
+    {NULL, NULL}
+};
+
+enum MAX6958Registers {
+    NoOp = 0x00,
+    DecodeMode = 0x01,
+    Intensity = 0x02,
+    ScanLimit = 0x03,
+    Configuration = 0x04,
+    FactoryReserved = 0x05,
+    GpIo = 0x06,
+    DisplayTest = 0x07,
+    ReadKeyDebounced = 0x08,
+    ReadKeyPressed = 0x0C,
+    Digit0 = 0x20,
+    Digit1 = 0x21,
+    Digit2 = 0x22,
+    Digit3 = 0x23,
+    Segments = 0x24
+};
+
+static const char* getDeviceNameForI2cAddress(uint8_t addr) {
+    PI2cDevice pKnownDevices = KnownDevices;
+    while (pKnownDevices->name != NULL) {
+        if (pKnownDevices->address == addr) {
+            return pKnownDevices->name;
+        }
+        pKnownDevices++;
+    }
+
+    return "<UNKNOWN>";
+}
+
+static const char* getNameForPostcode(uint16_t code) {
+    PPostCode pPostCodes = PostCodes;
+    while (pPostCodes->name != NULL) {
+        if (pPostCodes->code == code) {
+            return pPostCodes->name;
+        }
+        pPostCodes++;
+    }
+
+    return NULL;
+}
+
+static const char *getNameForMAX6958Register(uint8_t reg) {
+    switch(reg) {
+        case NoOp:
+            return "NoOp";
+        case DecodeMode:
+            return "DecodeMode";
+        case Intensity:
+            return "Intensity";
+        case ScanLimit:
+            return "ScanLimit";
+        case Configuration:
+            return "Configuration";
+        case FactoryReserved:
+            return "FactoryReserved";
+        case GpIo:
+            return "GpIo";
+        case DisplayTest:
+            return "DisplayTest";
+        case ReadKeyDebounced:
+            return "ReadKeyDebounced";
+        case ReadKeyPressed:
+            return "ReadKeyPressed";
+        case Digit0:
+            return "Digit0";
+        case Digit1:
+            return "Digit1";
+        case Digit2:
+            return "Digit2";
+        case Digit3:
+            return "Digit3";
+        case Segments:
+            return "Segments";
+        default:
+            return "<UNKNOWN>";
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,6 @@
 #include "common.h"
 #include "display.h"
 
-// If you need to use different I/O pins for I2C, change it here
-#define PIN_SDA_XBOX 0 // GPIO0
-#define PIN_SCL_XBOX 1 // GPIO1
-// ---
-
-/* DISPLAY */
-#define PIN_SDA_DISP 6 // GPIO6
-#define PIN_SCL_DISP 7 // GPIO7
-#define DISP_SCREEN_WIDTH 128
-#define DISP_SCREEN_HEIGHT 32
-#define SSD1306_DISP_ADDRESS 0x3C
-
-/* MAX6958 emulation */
-#define MAX6958_ADDRESS 0x38
-#define MAX6958_REGISTER_SIZE 0x25
-
-/* POST Code storage */
-#define POST_MAX_QUEUE_SIZE 30
-
-#define CTRL_C 3
-
-// State machine states
-enum State {
-    STATE_RETURN_TO_REPL,
-    STATE_REPL,
-    STATE_POST_MONITOR,
-    STATE_I2C_SCAN
-};
-
 State currentState = STATE_POST_MONITOR;
 String inputBuffer = "";
 uint8_t registers[MAX6958_REGISTER_SIZE];
@@ -38,105 +9,6 @@ uint8_t registers[MAX6958_REGISTER_SIZE];
 // Create a queue for POST codes
 cppQueue	postCodeQueue(sizeof(uint16_t), POST_MAX_QUEUE_SIZE, FIFO);
 Display     display(DISP_SCREEN_WIDTH, DISP_SCREEN_HEIGHT, PIN_SDA_DISP, PIN_SCL_DISP, SSD1306_DISP_ADDRESS, &Wire1);
-
-typedef struct {
-    uint16_t code;
-    const char *name;
-} PostCode, *PPostCode;
-
-typedef struct {
-    uint8_t address;
-    const char *name;
-} I2cDevice, *PI2cDevice;
-
-I2cDevice KnownDevices[] = {
-    {0x15, "V_NBCORE"},
-    {0x16, "V5_P0"},
-    {0x18, "V_MEMIOAB"},
-    {0x19, "V_MEMIOCD"},
-    {0x20, "V_GFXCORE / V_CPUCORE"},
-    {0x21, "V_MEMPHY/V_MEMIO/V_SOC"},
-    {0x24, "GPIO EXPANDER (UNUSED)"},
-
-    {0x2D, "V_DRAM1P8 DEBUG (UNUSED)"},
-    {0x2E, "V_SOC1P8 DEBUG (UNUSED)"},
-    {0x2F, "V_SOCPHY DEBUG (UNUSED)"},
-
-    {0x40, "RAMVPP MONITOR (UNUSED)"},
-    {0x41, "M2 MONITOR (UNUSED)"},
-    {0x42, "CFEXPRESS MONITOR (UNUSED)"},
-    {0x44, "12P0 MONITOR (UNUSED)"},
-    {0x45, "12P0_SB MONITOR (UNUSED)"},
-    {0x4A, "V_SOC1P8 MONITOR (UNUSED)"},
-    {0x4B, "V_SOCPHY MONITOR (UNUSED)"},
-
-    {0x38, "MAX6958/9A"},
-    {0x39, "MAX6958/9B"},
-    
-    {0x5A, "RF Unit (ISD9160)"},
-    {NULL, NULL}
-};
-
-PostCode PostCodes[] = {
-    {0x14FF, "SUCCESS"},
-    {0x0075, "SUCCESS"},
-    {NULL, NULL}
-};
-
-enum MAX6958Registers {
-    NoOp = 0x00,
-    DecodeMode = 0x01,
-    Intensity = 0x02,
-    ScanLimit = 0x03,
-    Configuration = 0x04,
-    FactoryReserved = 0x05,
-    GpIo = 0x06,
-    DisplayTest = 0x07,
-    ReadKeyDebounced = 0x08,
-    ReadKeyPressed = 0x0C,
-    Digit0 = 0x20,
-    Digit1 = 0x21,
-    Digit2 = 0x22,
-    Digit3 = 0x23,
-    Segments = 0x24
-};
-
-const char *getNameForRegister(uint8_t reg) {
-    switch(reg) {
-        case NoOp:
-            return "NoOp";
-        case DecodeMode:
-            return "DecodeMode";
-        case Intensity:
-            return "Intensity";
-        case ScanLimit:
-            return "ScanLimit";
-        case Configuration:
-            return "Configuration";
-        case FactoryReserved:
-            return "FactoryReserved";
-        case GpIo:
-            return "GpIo";
-        case DisplayTest:
-            return "DisplayTest";
-        case ReadKeyDebounced:
-            return "ReadKeyDebounced";
-        case ReadKeyPressed:
-            return "ReadKeyPressed";
-        case Digit0:
-            return "Digit0";
-        case Digit1:
-            return "Digit1";
-        case Digit2:
-            return "Digit2";
-        case Digit3:
-            return "Digit3";
-        case Segments:
-            return "Segments";
-        default:
-            return "<UNKNOWN>";
-    }
-}
 
 void receiveEvent(int howMany) {
     int reg = -1;
@@ -149,7 +21,7 @@ void receiveEvent(int howMany) {
             reg = Wire.read();
         } else {
             registers[reg] = Wire.read();
-            // Serial.printf("Register %s (0x%02x): 0x%02x\r\n", getNameForRegister(reg), reg, registers[reg]);
+            // Serial.printf("Register %s (0x%02x): 0x%02x\r\n", getNameForMAX6958Register(reg), reg, registers[reg]);
             if (reg == Segments) {
                 // Signal that we have a new POST code
                 uint16_t code = 0;
@@ -171,30 +43,6 @@ void receiveEvent(int howMany) {
     }
 }
 
-const char* getNameForPostcode(uint16_t code) {
-    PPostCode pPostCodes = PostCodes;
-    while (pPostCodes->name != NULL) {
-        if (pPostCodes->code == code) {
-            return pPostCodes->name;
-        }
-        pPostCodes++;
-    }
-
-    return NULL;
-}
-
-const char* getDeviceNameForAddress(uint8_t addr) {
-    PI2cDevice pKnownDevices = KnownDevices;
-    while (pKnownDevices->name != NULL) {
-        if (pKnownDevices->address == addr) {
-            return pKnownDevices->name;
-        }
-        pKnownDevices++;
-    }
-
-    return "<UNKNOWN>";
-}
-
 void scanForAvailableDevices() {
     Wire.begin();
     int error = 0;
@@ -208,7 +56,7 @@ void scanForAvailableDevices() {
         Wire.beginTransmission(addr);
         error = Wire.endTransmission();
 
-        const char *deviceName = getDeviceNameForAddress(addr);
+        const char *deviceName = getDeviceNameForI2cAddress(addr);
         if (error == 0)
         {
             Serial.printf("- Address: %i (hex: 0x%02x) [%s]\r\n", addr, addr, deviceName);
@@ -297,26 +145,6 @@ void handleRepl() {
     }
 }
 
-void setup() {
-    // Wait for serial to be ready
-    while (!Serial) {
-        delay(10);
-    }
-    Serial.begin(115200);
-    Wire.setSDA(PIN_SDA_XBOX);
-    Wire.setSCL(PIN_SCL_XBOX);
-
-    if (display.setup()) {
-        Serial.println("SSD1306 Display detected :)");
-    } else {
-        Serial.println("No display detected :(");
-    }
-    print("Firmware", __FW_VERSION__, 1000);
-    
-    Serial.println("POST Reader I2C");
-    startPostMonitor();
-}
-
 void printRegisters() {
     Serial.println(">> REGISTERS");
     for (int i = 0; i < MAX6958_REGISTER_SIZE; i++) {
@@ -339,6 +167,26 @@ void printCode(uint16_t code) {
     }
 
     Serial.println();
+}
+
+void setup() {
+    // Wait for serial to be ready
+    while (!Serial) {
+        delay(10);
+    }
+    Serial.begin(115200);
+    Wire.setSDA(PIN_SDA_XBOX);
+    Wire.setSCL(PIN_SCL_XBOX);
+
+    if (display.setup()) {
+        Serial.println("SSD1306 Display detected :)");
+    } else {
+        Serial.println("No display detected :(");
+    }
+    print("Firmware", __FW_VERSION__, 1000);
+    
+    Serial.println("POST Reader I2C");
+    startPostMonitor();
 }
 
 uint16_t code = 0;
